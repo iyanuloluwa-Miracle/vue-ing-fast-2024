7379e086cf07a94323019a380f1d34b899fee86b
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-08-08 22:24:08 +0100

    :sparkles: feat: added spinners

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,32 +1,33 @@
 <script setup>
-import {RouterLink} from 'vue-router';
+import { RouterLink } from 'vue-router';
 import JobListing from "./JobListing.vue";
-
-import { ref, defineProps, onMounted } from "vue";
+import { defineProps, onMounted, reactive } from "vue";
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
 import axios from 'axios';
 
 defineProps({
-  limit: Number,
-  showButton: {
-    type: Boolean,
-    default: false
-  }
+    limit: Number,
+    showButton: {
+        type: Boolean,
+        default: false
+    }
 });
 
-const jobs = ref([]);
+const state = reactive({
+    jobs: [],
+    isLoading: true
+});
 
-onMounted(async() =>{
-    try{
-        const response = await axios.get('http://localhost:8000/jobs')
-
-        jobs.value = response.data;
-
-    } catch(error){
-        console.error('Error Fetching Jobs', error)
-
+onMounted(async () => {
+    try {
+        const response = await axios.get('http://localhost:8000/jobs');
+        state.jobs = response.data;
+    } catch (error) {
+        console.error('Error Fetching Jobs', error);
+    } finally {
+        state.isLoading = false;
     }
-
-})
+});
 </script>
 
 <template>
@@ -35,14 +36,21 @@ onMounted(async() =>{
             <h2 class="text-3xl font-bold text-[#660e60] mb-6 text-center">
                 Browse Jobs
             </h2>
-        </div>
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-            <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
+
+            <!-- show loading spinner while loading is true-->
+            <div v-if="state.isLoading" class="text-center text-[#f012e1] py-6">
+                <PulseLoader/>
+            </div>
+            <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job" />
+            </div>
         </div>
     </section>
 
     <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
         <RouterLink to="/jobs"
-            class="block bg-[#660e60] text-white text-center py-4 px-6 rounded-xl hover:bg-[#893f71]">View All Jobs</RouterLink>
+            class="block bg-[#660e60] text-white text-center py-4 px-6 rounded-xl hover:bg-[#893f71]">
+            View All Jobs
+        </RouterLink>
     </section>
 </template>
